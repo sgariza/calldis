7662e135e563920dbd43c0e1f740a53c3bd96d56
sgariza <sgariza@gmail.com>
2026-01-22 10:56:34 +0100

    Update database connection to use environment variables

--- html/screen/config.php
+++ html/screen/config.php
@@ -117,7 +117,8 @@ $config = [
     'colorAlert' => '#D40000', // Color de alerta
 ];
 //echo ($_FILES[]);
-$db = new SQLite3('/var/www/html/screen/db.db');
+$dbfile = $getenv('DB_PATH') . $getenv('DB_FILE') ?? '/var/www/html/screen/db.db';
+$db = new SQLite3($dbfile);
 $res=$db->query("SELECT * FROM opciones");
 while ($row = $res->fetchArray(SQLITE3_ASSOC)) {
     if ($row['valor'] != ""){
