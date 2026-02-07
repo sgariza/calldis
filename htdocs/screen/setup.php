f22e402000aba014fe7e83e7258e9d8a5c9d45ef
sgariza <sgariza@gmail.com>
2026-02-07 16:57:03 +0100

    Add initial setup page for configuration

--- htdocs/screen/setup.php
+++ htdocs/screen/setup.php
@@ -1,1 +1,26 @@
-
+<!DOCTYPE html>
+<html>
+    <head>
+      <meta charset=utf-8 />
+      <title>Configuración inicial.</title>
+    </head>
+    <body>
+<?php
+include_once('config.php');
+$sql = "SELECT * FROM config";
+while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
+  $$row['opcion'] = $row['valor'];
+}
+if ($configured == 'true') {
+  echo ("Sistema iniciado.<br>\nNo se requiere acción.");
+}
+else {
+?>
+  <form>
+    <input type="text" label="URL" />
+  </form>
+<?php
+}
+?>
+  </body>
+</html>
